f637298deefe94573e5103e0b8295bf6c491f128
Onohije <igbadumeonoh@gmail.com>
2023-09-21 03:48:12 -0700

    AirBnB clone

--- models/user.py
+++ models/user.py
@@ -1,31 +1,11 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
+from models.base_model import BaseModel
 
 
-class User(BaseModel, Base):
+class User(BaseModel):
     """This class defines a user by various attributes"""
-
-    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-
-        places = relationship('Place', cascade='all, delete',
-                              backref='user')
-        reviews = relationship('Review', cascade='all, delete',
-                               backref='user')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-        places = []
-        reviews = []
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
