c60d1c856b4f5605c95259927a258929f0bb688a
fessycode <fessydoris@gmail.com>
2023-09-21 20:41:24 -0700

    update DB

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+""" Amenities Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Amenties Class """
+    if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
