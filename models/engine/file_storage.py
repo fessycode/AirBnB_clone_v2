13e4a8105fc68b4448bb3e2d80e041b128264342
fessycode <fessydoris@gmail.com>
2023-09-21 20:08:41 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""The module defines a class to manage file storage for hbnb clone"""
 import json
 
 
@@ -8,8 +8,14 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
+        if cls is not None:
+            objs = {}
+            for key, obj in self.all().items():
+                if isinstance(obj, cls):
+                    objs.update({key: obj})
+            return objs
         return FileStorage.__objects
 
     def new(self, obj):
@@ -45,6 +51,19 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes `obj` from `__objects` if it’s inside
+        if `obj` is equal to None, the method should not
+        do anything."""
+        if obj is not None:
+            key = obj.to_dict()['__class__'] + '.' + obj.id
+            del FileStorage.__objects[key]
+
+    def close(self):
+        """call reload() method for deserializing the JSON
+        file to objects"""
+        self.reload()
