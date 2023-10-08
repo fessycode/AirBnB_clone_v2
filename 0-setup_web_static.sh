73dbdce66724a43f64745060d544780e7dccd61b
fessycode <fessydoris@gmail.com>
2023-10-08 14:46:31 -0700

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,54 +1,27 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Update servers repository and install nginx
-apt update
-apt -y install nginx
-
-# create project directories
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-
-# create initial html
-cat << EOF | tee /data/web_static/releases/test/index.html
-<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-EOF
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# create a symlink to ..release/test/ dir
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# give ownership of /data/ to the current user
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-# set up nginx server configuration
-SERVER=$(hostname)
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-SERVER_CONFIG="server {
-        listen 80 default_server;
-        listen [::]:80 default_server;
-
-        root /var/www/html;
-        index index.html index.htm index.nginx-debian.html;
-
-        server_name _;
-
-        location / {
-                add_header X-Served-By '$SERVER';
-                try_files \$uri \$uri/ =404;
-        }
-
-	location /hbnb_static {
-                add_header X-Served-By '$SERVER';
-		alias /data/web_static/current;
-	}
-}"
-bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
-/etc/init.d/nginx restart
-
-exit 0
+sudo service nginx restart
